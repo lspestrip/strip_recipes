89898941b9024f2b2c74b187d9d571a0fadb9926
Maurizio Tomasi <maurizio.tomasi@unimi.it>
2017-05-18 11:25:14 +0200

    Improve the metadata describing the package

--- setup.py
+++ setup.py
@@ -11,14 +11,20 @@ setup(
     description='Create recipes for the LSPE/Strip tester software',
     long_description='''
 Python library to easily create complex recipes to be used with the LSPE/Strip
-tester software. All the Python control structures (if, while, for) can be used''',
+tester software. All the Python control structures (if, while, for) can be used.
+(LSPE is a balloon/ground experiment to search for the B-mode signal in the
+polarization pattern of the Cosmic Microwave Background. Strip is the low-frequency
+polarimetric instrument that will measure the sky from the ground.)''',
     author='Maurizio Tomasi',
     author_email='maurizio.tomasi@unimiREMOVETHIS.it',
     url='https://github.com/ziotom78/strip_recipes',
     license='MIT',
     classifiers=[
         'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers'
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering :: Astronomy',
+        'Topic :: Scientific/Engineering :: Physics',
+        'Topic :: Software Development :: Code Generators'
     ],
     keywords='cosmology laboratory',
     test_suite='nose.collector',
