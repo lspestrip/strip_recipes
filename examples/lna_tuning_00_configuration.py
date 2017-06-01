17ffb6a85b88f8a72e8e4d350db84c8f4719fb3a
Maurizio Tomasi <maurizio.tomasi@unimi.it>
2017-06-01 17:12:05 +0200

    Add a few examples which show how to use the code

---
+++ examples/lna_tuning_00_configuration.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+
+import sys
+
+from strip_recipes import RecipeFile
+
+PHASE_SWITCH_REVERSE_VOLTAGE = -1.8e3 # mV
+PHASE_SWITCH_FORWARD_CURRENT = 1.0 # mA
+ADC_BIAS = 1000 # ADU
+ADC_OFFSET = 0 # ADU
+
+
+def main(args):
+	if len(args) > 1:
+		print('''You cannot specify more than one command line parameter.
+If this parameter is present, it will be interpreted as the
+number of the polarimeter to be tested.
+''')
+
+	if len(args) == 1:
+		pol_number = int(args[0])
+	else:
+		pol_number = int(input('Enter the number of the polarimeter being tested: '))
+		
+	base_file_name = 'pol{0:02d}_lna_tuning_00_setup'.format(pol_number)
+	recipe_file_name = base_file_name + '.recipe'
+	
+	recipe = RecipeFile()
+	recipe.record_start(base_file_name)
+	recipe.sbs_on()
+
+	# Wait a few seconds to record the data and check that everything is ok
+	recipe.wait(10)
+	
+	# Set up the PH/SW
+	for phsw in ('A1', 'A2', 'B1', 'B2'):
+		recipe.bias_set('PS{0}_Vr'.format(phsw), PHASE_SWITCH_REVERSE_VOLTAGE)
+		recipe.bias_set('PS{0}_If'.format(phsw), PHASE_SWITCH_FORWARD_CURRENT)
+		# This is not implemented yet
+		# recipe.bias_set('PS{0}_Drive'.format(phsw), 0)
+
+	# Set up the ADCs
+	for adc in ('Q1', 'Q2', 'U1', 'U2'):
+		recipe.bias_set('{0}_bias'.format(adc), ADC_BIAS)
+		recipe.bias_set('{0}_offset'.format(adc), ADC_OFFSET)
+
+	# Wait a few seconds to record the data and check that everything is ok
+	recipe.wait(10)
+	recipe.sbs_off()
+	recipe.record_stop()
+
+	# Read this very script
+	with open(__file__, 'rt') as f:
+		this_script = f.readlines()
+
+	# Save the recipe and include this script in the comments
+	with open(recipe_file_name, "wt") as f:
+		recipe.write_to_file(f, comment_lines=['polarimeter = {0}'.format(pol_number)],
+		                     source_script=''.join(this_script))
+		
+	print('Recipe written to file "{0}"'.format(recipe_file_name))
+	
+if __name__ == '__main__':
+	main(sys.argv[1:])
---
+++ test.py
@@ -0,0 +1,28 @@
+from strip_recipes import RecipeFile
+
+recipe = RecipeFile()
+recipe.record_start('NOISE_TEMPERATURE')
+
+# Turn on the bias board
+recipe.sbs_on()
+
+# Set the temperature of the heater A to 20 K
+recipe.pid_set('LA', 20)
+
+# Wait some time (in seconds)
+recipe.wait(600)
+
+# Increase the temperature to 40 K
+recipe.pid_set('LA', 40)
+
+recipe.wait(600)
+
+recipe.sbs_off()
+recipe.record_stop()
+
+with open(__file__, 'rt') as f:
+    this_script = f.readlines()
+
+with open('recipe.txt', 'wt') as f:
+    recipe.write_to_file(f, comment=''.join(this_script))
+    
